43ed80980e3655c9f7fbdf0f12cc02451d446cdd
svm5 <sveta6328s@yandex.ru>
2023-02-16 20:57:31 +0300

    task3

--- 2.py
+++ 2.py
@@ -15,5 +15,13 @@ def proff(prof):
     return render_template("training.html", prof=prof.lower())
 
 
+@app.route("/list_prof/<list_type>")
+def list_proff(list_type):
+    if list_type.lower() not in ("ol", "ul"):
+        return "Некорректный параметр (Введите ol или ul)"
+    arr = ["Программист", "Доктор", "Инженер", "Пилот", "Эколог"]
+    return render_template("list_prof.html", prof_list=arr, list_type=list_type)
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080)
